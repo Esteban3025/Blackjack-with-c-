35b4bf51d5f4801749ed0c7bff880e5bbc5db619
Nosekp <glo2025pando@gmail.com>
2025-01-18 23:16:12 -0300

    Funcion de perder y ganar hecho con punteros

--- main.cpp
+++ main.cpp
@@ -11,12 +11,15 @@ std::mt19937 gen(rd());
 std::uniform_int_distribution<> dis(1, 21);
 
             
-int clave, option, option2;
+int clave, option;
+int * puntero;
 
 void menu();
 void game();
 void billetera();
 void findmesa();
+void lost();
+void win();
 
 // CLASE PARA EL JUGADOR
 class Player {
@@ -30,7 +33,7 @@ class Player {
 // CLASE PARA LA BANCA
 class Banca {
     public:
-    int bet[3] = {100, 1000, 10000};
+    int apuesta1 = 100, apuesta2 = 1000, apuesta3 = 10000;
     vector<int> cartas;
     int numdecartas;
     Banca(){}
@@ -47,7 +50,8 @@ void menu() {
     std::cin >> clave;
     if (clave == 1) {billetera();
     } else if (clave == 2) {game();
-    } else if (clave == 3) {std::cout << "Haz salido del casino: Haces bien!!";}
+    } else if (clave == 3) {std::cout << "Haz salido del casino: Haces bien!!";
+    } else {std::cout << "Opcion no valida, intenta de nuevo." << std::endl; menu();}
 } 
 
 
@@ -73,29 +77,100 @@ void billetera() {
     string str;
     std::cout << "Escribe \"exit\" para volver al menu" << std::endl;
     do {
-        std::getline(cin,str);
+        getline(cin,str);
     } while (str != "exit" && str != "EXIT");
     menu();
 }
 
 void findmesa() {
+    Player jugador;
+    Banca crupier;
     std::cout << "Crupier: Bienvenido al blackjack, elige tu apuesta para comenzar:"<< std::endl;
-    int apuesta;
     std::cout << "1. 100 $" << std::endl;
     std::cout << "2. 1000 $" << std::endl;
     std::cout << "3. 10000 $" << std::endl;
     std::cin >> option;
-    if (option == 1) {apuesta = 100;
-    } else if (option == 2) {apuesta = 1000;
-    } else if (option = 3) {apuesta = 10000;}
-    
+    /*
+    if (option == 1) {crupier.apuesta = 100;
+    } else if (option == 2) {crupier.apuesta = 1000;
+    } else if (option = 3) {crupier.apuesta = 10000;}
+    */
+    for (int i = 0; i < 2; i++)
+    {
+        crupier.cartas.push_back(dis(gen));
+        jugador.cartas.push_back(dis(gen));
+    }
+    cout << "Cartas del jugador: " << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        cout << jugador.cartas[i] << endl;   
+    }
+    cout << "Cartas de la banca: " << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        cout << crupier.cartas[i] << endl;
+    }
+
+    if (jugador.cartas[0] >= 21 || jugador.cartas[1] >= 21) {
+            lost();
+        }
 }
   
+void lost() {
+    Player jugador;
+    Banca crupier;
+    puntero = &crupier.apuesta1;
+    puntero = &crupier.apuesta2;
+    puntero = &crupier.apuesta3;
+    if (option == 1) {*puntero = crupier.apuesta1;
+    puntero = &jugador.cash;
+    *puntero = jugador.cash;
+    jugador.cash -= crupier.apuesta2;
+    cout << "Has perdido la apuesta, te quedas con: " << jugador.cash << "$" << endl;
+    } else if (option == 2) {*puntero = crupier.apuesta2;
+    puntero = &jugador.cash;
+    *puntero = jugador.cash;
+    jugador.cash -= crupier.apuesta3;
+    cout << "Has perdido la apuesta, te quedas con: " << jugador.cash << "$" << endl;
+    } else if (option = 3) { *puntero = crupier.apuesta3;
+    puntero = &jugador.cash;
+    *puntero = jugador.cash;
+    jugador.cash -= crupier.apuesta3;
+    cout << "Has perdido la apuesta, te quedas con: " << jugador.cash << "$" << endl;
+    }
+    menu();
+}
+
+
+void win() {
+    Player jugador;
+    Banca crupier;
+    puntero = &crupier.apuesta1;
+    puntero = &crupier.apuesta2;
+    puntero = &crupier.apuesta3;
+    if (option == 1) {*puntero = crupier.apuesta1;
+    puntero = &jugador.cash;
+    *puntero = jugador.cash;
+    jugador.cash += crupier.apuesta2;
+    cout << "Has perdido la apuesta, te quedas con: " << jugador.cash << "$" << endl;
+    } else if (option == 2) {*puntero = crupier.apuesta2;
+    puntero = &jugador.cash;
+    *puntero = jugador.cash;
+    jugador.cash += crupier.apuesta3;
+    cout << "Has perdido la apuesta, te quedas con: " << jugador.cash << "$" << endl;
+    } else if (option = 3) { *puntero = crupier.apuesta3;
+    puntero = &jugador.cash;
+    *puntero = jugador.cash;
+    jugador.cash += crupier.apuesta3;
+    cout << "Has perdido la apuesta, te quedas con: " << jugador.cash << "$" << endl;
+    }
+    menu();
+}
 
 //FUNCION PRINCIPAL DEL PROGRAMA
 int main() {
     std::cout << "****************************************" << std::endl;
-    std::cout << "            ¡¡¡BIENVENIDOS!!!           " << endl;
+    std::cout << "            BIENVENIDOS           " << endl;
     std::cout << "****************************************"<< std::endl;
     Player jugador;
     Banca crupier;
@@ -103,8 +178,6 @@ int main() {
         menu();
     } while (clave != 3);
     
-    
-
 }
 
 /*
